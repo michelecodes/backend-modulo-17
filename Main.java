d3cb1c185721ea031ff808470cd9eda861dbdf47
Michele Ribeiro <133832334+michelecodes@users.noreply.github.com>
2024-02-05 12:04:29 -0300

    Update Main.java

--- Main.java
+++ Main.java
@@ -15,9 +15,20 @@ public class Main {
         listaDeCarros.add(new Carro("Carro6", 2023, "Marca6", TipoCarro.PICAPES));
         listaDeCarros.add(new Carro("Carro7", 2017, "Marca7", TipoCarro.FURGAO));
 
+        // Adicionando mais carros à lista com diferentes marcas
+        listaDeCarros.add(new Carro("Civic", 2022, "Honda", TipoCarro.SEDA));
+        listaDeCarros.add(new Carro("Cruze", 2021, "GM", TipoCarro.SEDA));
+        listaDeCarros.add(new Carro("Sonata", 2023, "Hyundai", TipoCarro.SEDA));
+        listaDeCarros.add(new Carro("e-Tron", 2022, "Audi", TipoCarro.ESPORTIVO));
+        listaDeCarros.add(new Carro("Model S", 2020, "Tesla", TipoCarro.SEDA));
+        listaDeCarros.add(new Carro("Ecosport", 2019, "Ford", TipoCarro.SUV));
+        listaDeCarros.add(new Carro("BYD Tang", 2022, "BYD", TipoCarro.SUV));
+        listaDeCarros.add(new Carro("GLC", 2021, "Mercedes", TipoCarro.SUV));
+
         // Iterando pela lista e exibindo informações sobre os carros
         for (Carro carro : listaDeCarros) {
             System.out.println(carro);
         }
     }
 }
+
