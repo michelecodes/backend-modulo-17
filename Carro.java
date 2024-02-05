ce4fdcdf4bdd1b4e00b65301aed535a032b891f9
Michele Ribeiro <133832334+michelecodes@users.noreply.github.com>
2024-02-05 13:23:03 -0300

    Update and rename TipoDeSapato.java to Carro.java

---
+++ Carro.java
@@ -0,0 +1,25 @@
+class Carro {
+    private String marca;
+    private String modelo;
+    private double ano;
+
+    public Carro(String marca, String modelo, double ano) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.ano = ano;
+    }
+
+    public String getMarca() {
+        return marca;
+    }
+
+    public String getModelo() {
+        return modelo;
+    }
+
+    public double getAno() {
+        return ano;
+    }
+}
+
+
